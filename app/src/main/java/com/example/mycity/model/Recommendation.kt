0914e5625eeb0952814336d68463366bc0d44088
David Drinkard <dave@yardzen.com>
2026-07-22 17:18:37 -0400

    updates data classes and view model

---
+++ app/src/main/java/com/example/mycity/model/Recommendation.kt
@@ -0,0 +1,13 @@
+package com.example.mycity.model
+
+
+// Joshua David Drinkard
+// drinkarj@oregonstate.edu
+// CS 492
+
+data class Recommendation (
+    val id: Int,
+    val categoryId: Int,
+    val name: String,
+    val details: String
+)
--- app/src/main/java/com/example/mycity/ui/MyCityViewModel.kt
+++ app/src/main/java/com/example/mycity/ui/MyCityViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.mycity.ui
 import androidx.lifecycle.ViewModel
 import com.example.mycity.model.Category
+import com.example.mycity.model.Recommendation
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -14,6 +15,7 @@ class MyCityViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(MyCityUiState())
     val uiState: StateFlow<MyCityUiState> = _uiState.asStateFlow()
     private var nextId = 0
+    private var nextRecoId = 0
 
 // seed some data for development
 //    init {
@@ -48,9 +50,55 @@ class MyCityViewModel : ViewModel() {
         }
     }
 
+    fun addRecommendation(name: String, categoryId: Int) {
+        _uiState.update { currentState ->
+            if (
+               currentState.recommendations.any { it.name == name && it.categoryId == categoryId} ||
+                name == ""
+            ) {
+                currentState
+            } else {
+                val newRecommendation = Recommendation(
+                    id = nextRecoId++,
+                    name = name,
+                    categoryId = categoryId,
+                    details = ""
+                )
+                currentState.copy(
+                    recommendations = currentState.recommendations + newRecommendation
+                )
+            }
+        }
+    }
+
+    fun updateRecommendationDetails(recommendation: Recommendation, text: String) {
+        _uiState.update { currentState ->
+            if (
+                text == ""
+            ) {
+                currentState
+            } else {
+                currentState.copy(
+                    recommendations = currentState.recommendations.map { reco ->
+                        if (reco.id == recommendation.id) reco.copy(details = text) else reco
+                    }
+                )
+            }
+        }
+    }
+
+    fun deleteRecommendation(recommendation: Recommendation) {
+        _uiState.update { currentState ->
+            currentState.copy(
+                recommendations = currentState.recommendations.filter { it.id != recommendation.id}
+            )
+        }
+    }
+
 }
 
 
 data class MyCityUiState(
-    val categories: List<Category> = emptyList()
+    val categories: List<Category> = emptyList(),
+    val recommendations: List<Recommendation> = emptyList()
 )
